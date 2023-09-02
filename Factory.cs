87662659b2904ca80997df629ff3fa094ecef9a8
MinecraftDawn <eric4902077@gmail.com>
2023-09-02 23:23:43 +0800

    Factory Pattern

---
+++ Factory.cs
@@ -0,0 +1,54 @@
+﻿namespace Factory {
+    interface IProduct {
+        string Name { get; set; }
+
+        int Price {  get; set; }
+    }
+
+    class FrenchFries : IProduct {
+        public string Name { get; set; } = "薯條";
+        public int Price { get; set; } = 30;
+    }
+
+    class FireChicken : IProduct {
+        public string Name { get; set; } = "炸雞";
+        public int Price { get; set; } = 35;
+    }
+
+    class SimpleFactory {
+        public static IProduct Create(string name) {
+            switch(name) {
+                case "薯條": {
+                        Console.WriteLine("Simple FrenchFries");
+                        return new FrenchFries();
+                    }
+                case "炸雞": {
+                        Console.WriteLine("Simple FireChicken");
+                        return new FireChicken();
+                    }
+                default: {
+                        return null;
+                    }
+            }
+        }
+    }
+
+
+    interface IFactory {
+        IProduct Create();
+    }
+
+    class FrenchFriesFactory : IFactory {
+        public IProduct Create() {
+            Console.WriteLine("FrenchFriesFactory");
+            return new FrenchFries();
+        }
+    }
+
+    class FireChickenFactory : IFactory {  
+        public IProduct Create() {
+            Console.WriteLine("FireChickenFactory");
+            return new FireChicken();
+        } 
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Singleton;
-
+using Factory;
 
 Console.WriteLine(123);
-
+Singleton.SignletonLazySafe.getInstance();
+Console.WriteLine(Factory.SimpleFactory.Create("薯條").Name);
+Console.WriteLine((new Factory.FireChickenFactory()).Create().Name);
