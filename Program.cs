3cde88832e11d130ec1bff945ff63ed454c3d9f1
MinecraftDawn <eric4902077@gmail.com>
2023-09-03 11:45:02 +0800

    Strategy

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 using Singleton;
 using Factory;
+using Strategy;
 
 Console.WriteLine(123);
 Singleton.SignletonLazySafe.getInstance();
 Console.WriteLine(Factory.SimpleFactory.Create("薯條").Name);
 Console.WriteLine((new Factory.FireChickenFactory()).Create().Name);
+List<Strategy.Item> items  = new List<Strategy.Item>();
+items.Add(new Strategy.Item(0, "A"));
+items.Add(new Strategy.Item(2, "C"));
+items.Add(new Strategy.Item(1, "B"));
+
+
+items.Sort((new Strategy.SortByName()));
+foreach (Strategy.Item item in items) {
+    Console.WriteLine(item.Name);
+}
+
---
+++ Strategy.cs
@@ -0,0 +1,28 @@
+﻿namespace Strategy {
+    class Item {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+
+        public Item() {
+
+        }
+
+        public Item(int id, string name) {
+            Id = id;
+            Name = name;
+        }
+    }
+    class SortById : Comparer<Item> {
+        public override int Compare(Item x, Item y) {
+            if (x.Id < y.Id) return -1;
+            if (x.Id > y.Id) return 1;
+            return 0;
+        }
+    }
+
+    class SortByName : Comparer<Item> {
+        public override int Compare(Item x, Item y) {
+            return String.Compare(x.Name, y.Name);
+        }
+    }
+}
